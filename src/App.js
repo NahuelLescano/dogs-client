7b7c9269d6bd79634e1847145adb1f7f05ecf964
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-20 00:52:23 -0300

    Bringing my components and linking to it.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+
+import LandingPage from './components/LandingPage/LandingPage';
+import Home from './components/Home/Home';
+import Details from './components/Details/Details';
+import Form from './components/Form/Form';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <Routes>
+        <Route exact path='/' element={<LandingPage />} />
+        <Route exact path='/home' element={<Home />} />
+        <Route exact path='/details/:id' element={<Details />} />
+        <Route exact path='/form' element={<Form />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
