00c1d995619908a7890774e48b939b8753c1354f
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-21 16:41:07 -0300

    Minor changes.

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './LandingPage.css';
 
 export default function LandingPage() {
   return (
-    <div>
-      <h1>Bienvenido a la página de perros</h1>
-      <button>
-        <Link to="/home">Ingresar</Link>
-      </button>
+    <div className="container">
+      <div className="presentational">
+        <h1>Welcome to my dogs page</h1>
+        <Link to="/home">
+          <button className="button">Enter</button>
+        </Link>
+      </div>
     </div>
   );
 }
