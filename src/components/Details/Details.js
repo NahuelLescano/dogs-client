fd381805234133f6a8422303414572103bf684a4
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-21 16:40:43 -0300

    Fixing bugs.

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -5,31 +5,26 @@ const { REACT_APP_GET_ALL_DOGS } = process.env;
 
 export default function Details() {
   const { id } = useParams();
-  const [dogs, setDogs] = useState();
+  const [dog, setDog] = useState({});
 
   useEffect(() => {
     axios
       .get(`${REACT_APP_GET_ALL_DOGS}/id/${id}`)
-      .then((response) => setDogs(response.data));
+      .then((response) => setDog(response.data));
   }, [id]);
 
   return (
     <div>
-      <h1>Detalles del perro</h1>
-      {dogs &&
-        dogs.map((dog) => (
-          <div key={dog.id}>
-            <img src={dog.image?.url} alt={dog.name} />
-            <h2>{dog.id}</h2>
-            <h2>{dog.name}</h2>
-            <h2>{dog.height?.metric}</h2>
-            <h2>{dog.weight?.metric}</h2>
-            <h2>{dog.life_span}</h2>
-          </div>
-        ))}
-        <button>
-          <Link to='/home'>Atras</Link>
-        </button>
+      <h1>Dog details</h1>
+      <img src={dog.image} alt={dog.name} />
+      <h2>ID: {dog.id}</h2>
+      <h2>Name: {dog.name}</h2>
+      <h2>Height: {dog.height?.metric}</h2>
+      <h2>Weight: {dog.weight?.metric}</h2>
+      <h2>Life span: {dog.life_span}</h2>
+      <button>
+        <Link to="/home">Go back</Link>
+      </button>
     </div>
   );
 }
