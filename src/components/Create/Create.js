0167234eb008197e9263a0e46138aff7c3d1d94b
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-21 12:53:10 -0300

    Minor changes. It does not render error messages.

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './Create.css';
+import validation from './validation';
 
 const { REACT_APP_GET_TEMPERAMENTS } = process.env;
 
+// Todavía falta que se rendericen los errores.
 export default function Form() {
   const [input, setInput] = useState({
-    weight: '',
-    height: '',
+    weight: {
+      min: 0,
+      max: 0,
+    },
+    height: {
+      min: 0,
+      max: 0,
+    },
     name: '',
     lifeSpan: '',
     image: '',
@@ -16,16 +25,50 @@ export default function Form() {
 
   const [temperaments, setTemperaments] = useState();
 
-  const handleChange = (event) => {
+  const [errors, setErrors] = useState({
+    weight: '',
+    height: '',
+    name: '',
+    lifeSpan: '',
+    image: '',
+    temperament: '',
+  });
+
+  const handleInputChange = (event) => {
     setInput({
       ...input,
       [event.target.name]: event.target.value,
     });
+
+    setErrors(
+      validation({
+        ...input,
+        [event.target.name]: event.target.value,
+      })
+    );
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(input);
+    if (
+      !errors.weight &&
+      !errors.height &&
+      !errors.name &&
+      !errors.lifeSpan &&
+      !errors.image &&
+      !errors.temperament
+    ) {
+      setErrors(
+        validation({
+          weight: '',
+          height: '',
+          name: '',
+          lifeSpan: '',
+          image: '',
+          temperament: '',
+        })
+      );
+    }
   };
 
   useEffect(() => {
@@ -36,38 +79,50 @@ export default function Form() {
 
   return (
     <div>
+      <button>
+        <Link to="/home">Atras</Link>
+      </button>
       <h1>Crear perro</h1>
       <form className="form" onSubmit={handleSubmit}>
-        <label htmlFor="weight">Peso: </label>
-        <input
-          id="weight"
-          name="weight"
-          placeholder="Ingrese el peso"
-          onChange={handleChange}
-        />
+        <div className="minMax">
+          <label>Peso (cm): </label>
+          <label htmlFor="min">Mínimo: </label>
+          <input id="min" name="min" placeholder="Ingrese el peso min..." />
+          <label htmlFor="max">Máximo: </label>
+          <input
+            id="max"
+            name="max"
+            placeholder="Ingrese el peso max..."
+            onChange={handleInputChange}
+          />
+        </div>
         <label htmlFor="height">Altura: </label>
         <input
           id="height"
           name="height"
           placeholder="Ingrese la altura"
-          onChange={handleChange}
+          onChange={handleInputChange}
         />
         <label htmlFor="name">Nombre: </label>
         <input
           id="name"
           name="name"
           placeholder="Ingrese el nombre"
-          onChange={handleChange}
+          onChange={handleInputChange}
         />
         <label htmlFor="lifeSpan">Tiempo de vida: </label>
         <input
           id="lifeSpan"
           name="lifeSpan"
           placeholder="Ingrese el tiempo de vida"
-          onChange={handleChange}
+          onChange={handleInputChange}
         />
         <label htmlFor="temperament">Temperamento: </label>
-        <select id="temperament" name="temperament" onChange={handleChange}>
+        <select
+          id="temperament"
+          name="temperament"
+          onChange={handleInputChange}
+        >
           <option>Seleccione un temperamento</option>
           {temperaments &&
             temperaments.map((temp) => (
@@ -79,7 +134,7 @@ export default function Form() {
           id="image"
           name="image"
           placeholder="Ingrese la url de la imagen"
-          onChange={handleChange}
+          onChange={handleInputChange}
         />
         <button type="submit">Enviar</button>
       </form>
