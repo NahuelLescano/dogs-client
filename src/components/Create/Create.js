e0d0abea30b2888fbffc720644047124fad6a3fb
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-24 23:54:42 -0300

    Fixing some bugs, create does not work yet.

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -2,21 +2,17 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './Create.css';
-import validation from './validation';
+// import validation from './validation';
 
-const { REACT_APP_GET_TEMPERAMENTS } = process.env;
+const { REACT_APP_GET_TEMPERAMENTS, REACT_APP_GET_ALL_DOGS } = process.env;
 
 // Todavía falta que se rendericen los errores.
 export default function Form() {
   const [input, setInput] = useState({
-    weight: {
-      min: 0,
-      max: 0,
-    },
-    height: {
-      min: 0,
-      max: 0,
-    },
+    weightMin: '',
+    weightMax: '',
+    heightMin: '',
+    heightMax: '',
     name: '',
     lifeSpan: '',
     image: '',
@@ -25,14 +21,14 @@ export default function Form() {
 
   const [temperaments, setTemperaments] = useState();
 
-  const [errors, setErrors] = useState({
-    weight: '',
-    height: '',
-    name: '',
-    lifeSpan: '',
-    image: '',
-    temperament: '',
-  });
+  // const [errors, setErrors] = useState({
+  //   weight: '',
+  //   height: '',
+  //   name: '',
+  //   lifeSpan: '',
+  //   image: '',
+  //   temperament: '',
+  // });
 
   const handleInputChange = (event) => {
     setInput({
@@ -40,34 +36,47 @@ export default function Form() {
       [event.target.name]: event.target.value,
     });
 
-    setErrors(
-      validation({
-        ...input,
-        [event.target.name]: event.target.value,
-      })
-    );
+    // setErrors(
+    //   validation({
+    //     ...input,
+    //     [event.target.name]: event.target.value,
+    //   })
+    // );
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    if (
-      !errors.weight &&
-      !errors.height &&
-      !errors.name &&
-      !errors.lifeSpan &&
-      !errors.image &&
-      !errors.temperament
-    ) {
-      setErrors(
-        validation({
-          weight: '',
-          height: '',
-          name: '',
-          lifeSpan: '',
-          image: '',
-          temperament: '',
-        })
-      );
+    // if (
+    //   !errors.weight &&
+    //   !errors.height &&
+    //   !errors.name &&
+    //   !errors.lifeSpan &&
+    //   !errors.image &&
+    //   !errors.temperament
+    // ) {
+    //   setErrors(
+    //     validation({
+    //       weight: '',
+    //       height: '',
+    //       name: '',
+    //       lifeSpan: '',
+    //       image: '',
+    //       temperament: '',
+    //     })
+    //   );
+    // }
+    input.weight = {
+      metric: `${input.weightMin} - ${input.weightMax}`,
+    };
+
+    input.height = {
+      metric: `${input.heightMin} - ${input.heightMax}`,
+    };
+
+    try {
+      await axios.post(REACT_APP_GET_ALL_DOGS);
+    } catch (error) {
+      alert(error.message);
     }
   };
 
@@ -87,22 +96,37 @@ export default function Form() {
         <div className="minMax">
           <label>Peso (cm): </label>
           <label htmlFor="min">Mínimo: </label>
-          <input id="min" name="min" placeholder="Ingrese el peso min..." />
+          <input
+            id="min"
+            name="weightMin"
+            placeholder="Ingrese el peso min..."
+            onChange={handleInputChange}
+          />
           <label htmlFor="max">Máximo: </label>
           <input
             id="max"
-            name="max"
+            name="weightMax"
             placeholder="Ingrese el peso max..."
             onChange={handleInputChange}
           />
         </div>
+        <br/>
         <label htmlFor="height">Altura: </label>
+        <label htmlFor="min">Mínimo: </label>
         <input
-          id="height"
-          name="height"
-          placeholder="Ingrese la altura"
+          id="min"
+          name="heightMin"
+          placeholder="Ingrese la altura min..."
           onChange={handleInputChange}
         />
+        <label htmlFor="max">Máximo: </label>
+        <input
+          id="max"
+          name="heightMax"
+          placeholder="Ingrese el peso max..."
+          onChange={handleInputChange}
+        />
+        <br/>
         <label htmlFor="name">Nombre: </label>
         <input
           id="name"
@@ -110,6 +134,7 @@ export default function Form() {
           placeholder="Ingrese el nombre"
           onChange={handleInputChange}
         />
+        <br/>
         <label htmlFor="lifeSpan">Tiempo de vida: </label>
         <input
           id="lifeSpan"
@@ -117,6 +142,7 @@ export default function Form() {
           placeholder="Ingrese el tiempo de vida"
           onChange={handleInputChange}
         />
+        <br/>
         <label htmlFor="temperament">Temperamento: </label>
         <select
           id="temperament"
@@ -129,6 +155,7 @@ export default function Form() {
               <option key={temp.id}>{temp.name}</option>
             ))}
         </select>
+        <br/>
         <label htmlFor="image">Imagen: </label>
         <input
           id="image"
