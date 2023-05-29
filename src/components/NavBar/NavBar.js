b97627650ab969f6f57fab7e8247cdc43800e14b
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-29 00:38:35 -0300

    Adding filtering and sorting options.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { filteredDogs } from '../../redux/action-creators';
 
 export default function NavBar() {
+  const allTemperaments = useSelector((state) => state.temperaments);
+  // eslint-disable-next-line
+  const [dogFiltered, setDogFitered] = useState();
+  const dispatch = useDispatch();
+
+  const handleInputChange = (e) => {
+    dispatch(filteredDogs(e.target.value));
+  };
+
   return (
     <div className="nav-container">
       <NavLink
-        className={({ isActive }) => (!isActive && 'disable')}
+        className={({ isActive }) => !isActive && 'disable'}
         to="/create"
       >
         Create dog
       </NavLink>
+      <select name="temperament" onChange={handleInputChange}>
+        <option>Seleccione un temperamento</option>
+        {allTemperaments &&
+          allTemperaments.map((temp) => (
+            <option key={temp.id} value={temp.id}>
+              {temp.name}
+            </option>
+          ))}
+      </select>
+
+      <select name="create" onChange={handleInputChange}>
+        <option>api</option>
+        <option>base de dato</option>
+      </select>
+
+      <select>
+        <option>Ordenar</option>
+      </select>
     </div>
   );
 }
