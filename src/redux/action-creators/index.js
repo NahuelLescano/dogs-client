f5c547f84fe6bdabffaa3ae773701ef5ee543157
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-29 00:40:10 -0300

    Temperaments is global now, adding filtered dog option.

--- src/redux/action-creators/index.js
+++ src/redux/action-creators/index.js
@@ -1,6 +1,10 @@
-import { GET_ALL_DOGS } from '../action-types';
+import {
+  GET_ALL_DOGS,
+  DOGS_FILTERED,
+  GET_ALL_TEMPERAMENTS,
+} from '../action-types';
 import axios from 'axios';
-const { REACT_APP_GET_ALL_DOGS } = process.env;
+const { REACT_APP_GET_ALL_DOGS, REACT_APP_GET_TEMPERAMENTS } = process.env;
 
 export const getAllDogs = () => {
   return async (dispatch) => {
@@ -9,5 +13,22 @@ export const getAllDogs = () => {
       type: GET_ALL_DOGS,
       payload: response.data,
     });
+  };
+};
+
+export const getAllTemperaments = () => {
+  return async (dispatch) => {
+    const response = await axios.get(REACT_APP_GET_TEMPERAMENTS);
+    dispatch({
+      type: GET_ALL_TEMPERAMENTS,
+      payload: response.data,
+    });
+  };
+};
+
+export const filteredDogs = (fileredDogs) => {
+  return {
+    type: DOGS_FILTERED,
+    payload: fileredDogs,
   };
 };
