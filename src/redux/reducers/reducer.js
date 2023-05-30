5ed00e21e073da9632453e64b9603dca9bf5c034
nahuel <lescanoagustin10nahuel@gmail.com>
2023-05-30 00:51:00 -0300

    Adding the correspoding case to the reducer.

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -2,9 +2,13 @@ import {
   GET_ALL_DOGS,
   DOGS_FILTERED,
   GET_ALL_TEMPERAMENTS,
+  ERROR,
+  DOG_TEMPERAMENT,
+  DOG_RESET,
 } from '../action-types';
 
 const initialState = {
+  error: false,
   dogs: [],
   allDogs: [],
   temperaments: [],
@@ -20,14 +24,50 @@ const rootReducer = (state = initialState, { type, payload }) => {
       };
 
     case DOGS_FILTERED:
+      const myDogs = state.allDogs;
+      const dogs =
+        payload === 'api'
+          ? myDogs.filter((dog) => typeof dog.id === 'number')
+          : myDogs.filter((dog) => typeof dog.id === 'string');
+
+      const error = payload === 'base de datos' && dogs.length === 0;
+      if (!error) {
+        return {
+          ...state,
+          dogs,
+        };
+      }
+
       return {
         ...state,
+        error: true,
+      };
+
+    case DOG_TEMPERAMENT:
+      const dogTemper = state.dogs.filter((dog) =>
+        dog.temperament?.includes(payload)
+      );
+      return {
+        ...state,
+        allDogs: dogTemper,
       };
 
     case GET_ALL_TEMPERAMENTS:
       return {
         ...state,
         temperaments: payload,
+      };
+
+    case DOG_RESET:
+      return {
+        ...state,
+        allDogs: state.dogs,
+      };
+
+    case ERROR:
+      return {
+        ...state,
+        error: false,
       };
 
     default:
