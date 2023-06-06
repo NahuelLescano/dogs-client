94d9a09da83c02f4eeb162d306c6c3d2b95a91b3
nahuel <lescanoagustin10nahuel@gmail.com>
2023-06-06 00:40:47 -0300

    Adding a button to the link.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Card.css';
 
-export default function Card({ id, name, lifeSpan, image }) {
+export default function Card({ id, name, lifeSpan, temperament, image }) {
   return (
     <div className="dog-card">
-      <h2>{name}</h2>
-      <h3>{lifeSpan}</h3>
+      <h2>Name: {name}</h2>
+      <h3>Life span: {lifeSpan}</h3>
+      <select className="card-select">
+        <option value="">Temperaments</option>
+        {temperament.map((temp, index) => (
+          <option key={index}>{temp}</option>
+        ))}
+      </select>
       <img src={image} alt={name} className="dog-image" />
-      <NavLink
-        className={({ isActive }) => !isActive && 'disable-card'}
-        to={`/details/${id}`}
-      >
-        See more
-      </NavLink>
+      <button className="card-button">
+        <NavLink
+          className={({ isActive }) => !isActive && 'disable-card'}
+          to={`/details/${id}`}
+        >
+          See more
+        </NavLink>
+      </button>
     </div>
   );
 }
