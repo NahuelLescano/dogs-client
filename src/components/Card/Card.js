6985cccbc043f8f5343c47f9ca85f678f849e9ea
nahuel <lescanoagustin10nahuel@gmail.com>
2023-06-11 23:03:49 -0300

    Now temperament is rendered as h3 instead of a select.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,13 +7,13 @@ export default function Card({ id, name, lifeSpan, temperament, image }) {
     <div className="dog-card">
       <h2>Name: {name}</h2>
       <h3>Life span: {lifeSpan}</h3>
-      <select className="card-select">
-        <option value="">Temperaments</option>
+      <h3>Temperaments: </h3>
+      <div className="temper-info">
         {temperament &&
           temperament.map((temp, index) => (
             <option key={index}>{temp.name ? temp.name : temp}</option>
           ))}
-      </select>
+      </div>
       <img src={image} alt={name} className="dog-image" />
       <button className="card-button">
         <NavLink
